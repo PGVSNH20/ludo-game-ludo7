296b4a9671b2cbc8a42b84af1321c34b728dbb39
Marcus Broman <mbroman23@gmail.com>
2021-03-30 13:51:12 +0200

    Added projects to solution

---
+++ Source/LudoGame7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LudoGame7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

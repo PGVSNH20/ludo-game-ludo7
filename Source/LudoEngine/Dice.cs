743c17fb0b81d16b65babd1c21cfa2496c298f27
Kire <70213370+kireyperez@users.noreply.github.com>
2021-03-30 15:12:30 +0200

    Added dice method and strings in Player class.

--- Source/LudoEngine/Dice.cs
+++ Source/LudoEngine/Dice.cs
@@ -4,5 +4,11 @@ namespace LudoEngine
 {
     public class Dice
     {
+        Random rnd = new Random();
+        public int Roll()
+        {
+            return rnd.Next(1, 7);
+        }
+
     }
 }
---
+++ Source/LudoEngine/Players.cs
@@ -0,0 +1,5 @@
+class Player
+{
+    public string Name;
+    public string Color;
+}
